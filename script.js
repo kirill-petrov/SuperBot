bf3fbe5c0389cd422a616d2f6964216e36dfd1b4
Kirill Petrov <kira.mailbox@ya.ru>
2021-04-06 10:30:51 +0300

    r1

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const form = document.querySelector('.bot__form form');
+const input = document.querySelector('input');
+const dataBase = {
+  'Как дела': 'Хорошо! Как сам?',
+  'Как погода': 0,
+};
+const botAnswer = document.querySelector('#bot__answer');
+const animationType = 'animate__headShake';
 
-form.addEventListener('submit', event => {
-  event.preventDefault()
-  const value = event.target.input.value
-  return console.log(value);
-})
+const handleAnimationEnd = () => botAnswer.classList.remove(animationType);
 
-console.log(form);
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const value = e.target.question.value;
+  const answer = dataBase[value];
+
+  if (value) {
+    if (answer !== undefined) {
+      botAnswer.innerText = answer;
+    } else {
+      botAnswer.innerText = 'Я не знаю ответ на данный вопрос.';
+    }
+
+    botAnswer.classList.add(animationType);
+    botAnswer.addEventListener('animationend', handleAnimationEnd, {
+      once: true,
+
+    });
+    input.value = '';
+  }
+});
